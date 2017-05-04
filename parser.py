f2bdd6f351a95c135fc12259b8524616fd12f943
Felipe Bonet <fbonet@boletinoficial.gov.ar>
2017-05-04 11:01:28 -0300

    Separo la salida. Parametrizo numero de ejercicio.

--- parser.py
+++ parser.py
@@ -12,56 +12,80 @@ class Parser(object):
 	    except ValueError:
 	        return False
 
-	def parse(self, filepath, train_pct, test_pct, validation_pct):
-		#try:
-			reader = csv.reader(open(filepath, 'r'))
-			cant_datos = len(open(filepath).readlines())
+	def parse(self, nro_ejercicio, train_pct, test_pct, validation_pct):
 
-			# Estimo cantidad de registros que ira a cada set de datos
-			cant_datos_training = int(cant_datos * float(train_pct) / 100)
-			cant_datos_test = int(cant_datos * float(test_pct) / 100)
-			cant_datos_validation = int(cant_datos * float(validation_pct) / 100)
+		if nro_ejercicio == '1':
+			filepath = 'tp1_ej1_training.csv'
+		else:
+			filepath = 'tp1_ej2_training.csv'
 
-			datos = []
-			datos_train = []
-			datos_test = []
-			datos_validation = []
+		reader = csv.reader(open(filepath, 'r'))
+		cant_datos = len(open(filepath).readlines())
 
-			# Paso CSV a lista de rows para manejar mas facil
-			for row in reader:
-				values = []
-				for value in row:
-					if self.is_numberic(value):
-						value = float(value)
-					elif value == 'B':
-						value = 1
-					elif value == 'M':
-						value = -1
+		# Estimo cantidad de registros que ira a cada set de datos
+		cant_datos_training = int(cant_datos * float(train_pct) / 100)
+		cant_datos_test = int(cant_datos * float(test_pct) / 100)
+		cant_datos_validation = int(cant_datos * float(validation_pct) / 100)
 
-					values.append(value)
-				datos.append(values)
+		datos = []
+		datos_train = []
+		datos_test = []
+		datos_validation = []
 
-			# Mezclo datos para que la seleccion y division sean azarosas
-			random.shuffle(datos)
+		# Paso CSV a lista de rows para manejar mas facil
+		for row in reader:
+			values = []
+			for value in row:
+				if self.is_numberic(value):
+					value = float(value)
+				elif value == 'B':
+					value = 1
+				elif value == 'M':
+					value = -1
+				values.append(value)
 
-			i = 0
-			for row in datos:
-			# Divido los datos del input en porciones respectivas de training, test y validation
-				index_training = (0, cant_datos_training)
-				index_test = (index_training[1], index_training[1] + cant_datos_test)
-				index_validation = (index_test[1], index_test[1] + cant_datos_validation)
+			datos.append(values)
 
-				if i in range(*index_training):
-					datos_train.append(row)
-				elif i in range(*index_test):
-					datos_test.append(row)
-				elif i in range(*index_validation):
-					datos_validation.append(row)
+		# Mezclo datos para que la seleccion y division sean azarosas
+		random.shuffle(datos)
 
-				i += 1
+		row_number = 0
+		for row in datos:
+		# Divido los datos del input en porciones respectivas de training, test y validation
+			index_training = (0, cant_datos_training)
+			index_test = (index_training[1], index_training[1] + cant_datos_test)
+			index_validation = (index_test[1], index_test[1] + cant_datos_validation)
 
-			return datos_train, datos_validation, datos_test
+			# Separo la salida de cada row. Ahora row con salida sera una tupla
+			# Donde el primer elemento es un array de datos y el segundo es el resultado
+			# Para el ej1, el resultado es un B = 1 o M = -1
+			# Para el ej2, el resultado es una tupla de valores, uno para cada carga
+			row_con_salida = self.separar_salida(nro_ejercicio, row)
 
-		#except Exception as e:
-		   #print "Error al parsear archivo de entrada"
-		   #print str(e)
+			if row_number in range(*index_training):
+				datos_train.append(row_con_salida)
+			elif row_number in range(*index_test):
+				datos_test.append(row_con_salida)
+			elif row_number in range(*index_validation):
+				datos_validation.append(row_con_salida)
+
+			row_number += 1
+
+		return datos_train, datos_validation, datos_test
+
+	def separar_salida(self, nro_ejercicio, row):
+		# Se parsea distinto si es el ejercicio 1 o el 2
+		# En el ejercicio 1 los resultados son B o M (maligno/benigno)
+		# y la salida es 1 columna
+		if nro_ejercicio == '1':
+			salida = row[0]
+			del row[0]
+			return (row, salida)
+
+		# En el ejercicio 2 los resultados son B o M (maligno/benigno)
+		# y la salida es 1 columna
+		elif nro_ejercicio == '2':
+			salida = (row[-2], row[-1])
+			del row[-2]
+			del row[-1]
+			return (row, salida)
--- script.py
+++ script.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 import argparse
-import os
+import os, sys
 import parser as psr
 import perceptron as ppn
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 def iniciar():
     usage = 'Este script tiene un único parametro obligatorio, que es el path del archivo de entrada con los datos de input \n' \
           'Todos los demás son opcionales.\n' \
           'Ejemplo de ejecución: \n' \
-          '$ python perceptron.py tp1_ej1_training.csv -ep=10000 -eta=0.01 -tr=20 -te=30 -val=50'
+          '$ python perceptron.py 1 -ep=10000 -eta=0.01 -tr=20 -te=30 -val=50'
 
     parser = argparse.ArgumentParser(usage=usage)
 
     # Argumento obligatorio: archivo de entrada
-    parser.add_argument("input_file", type=str, help='Path del archivo con datos de entrada')
+    parser.add_argument("nro_ejercicio", type=str, help='Numero de ejercicio. Valores: 1/2')
 
     # Argumentos opcionales: cantidad de epocas, eta, y proporcion de los datos
     # para usar como entrenamiento, test y validacion
@@ -26,7 +26,7 @@ def iniciar():
 
     args = parser.parse_args()
 
-    input_file = args.input_file
+    nro_ejercicio = args.nro_ejercicio
     eta = float(args.eta)
     epochs = int(args.epochs)
     train_pct = float(args.train)
@@ -35,47 +35,45 @@ def iniciar():
 
     os.system('clear')
     print 'TP1 - Perceptrón Multicapa'
-    print "Se intentará procesar los datos en "+input_file+" ejecutando "+str(epochs)+" épocas con ETA "+str(eta)
+    print "Se intentará procesar los datos del ejercicio "+nro_ejercicio+" ejecutando "+str(epochs)+" épocas con ETA "+str(eta)
     print str(train_pct) + "% del input utilizado como Entrenamiento"
     print str(test_pct) + "% del input utilizado como Testing"
     print str(validation_pct) + "% del input utilizado como Validacion"
     print '-------------------------------------------------------------------------'
 
-    return input_file, eta, epochs, train_pct, test_pct, validation_pct
+    return nro_ejercicio, eta, epochs, train_pct, test_pct, validation_pct
 
 ######### INICIO SCRIPT ##############
 
 # Ejemplo de ejecucion:
 
-input_file, eta, epochs, train_pct, test_pct, validation_pct = iniciar()
+nro_ejercicio, eta, epochs, train_pct, test_pct, validation_pct = iniciar()
 
 i = psr.Parser()
-datos_train, datos_validation, datos_test = i.parse(input_file, train_pct, test_pct, validation_pct)
+datos_train, datos_validation, datos_test = i.parse(nro_ejercicio, train_pct, test_pct, validation_pct)
 
 # Ejemplo de train
 DATOS = datos_train
 
-N_ENTRADA = len(DATOS[0]) - 1
-N_SALIDA = len(set([row[0] for row in DATOS]))
+N_ENTRADA = len(DATOS[0][0]) - 1
+RESULTADOS_ESPERADOS = [row[-1] for row in DATOS]
 
-PPN = ppn.PerceptronMulticapa(N_ENTRADA, [3], 2, funcion_activacion="logistica", distribucion_pesos="normal", momentum=0)
-PPN.train_batch(DATOS, N_SALIDA, eta=eta, epochs=epochs, tamanio_muestra_batch=1)
+PPN = ppn.PerceptronMulticapa(N_ENTRADA, [3], 1, funcion_activacion="tangente", distribucion_pesos="normal", momentum=0)
+PPN.train([row[0] for row in DATOS], RESULTADOS_ESPERADOS, eta=0.5, epochs=50, tamanio_muestra_batch=1)
 
-DATOS_PREDICCION = datos_validation
+DATOS_PREDICCION = datos_train
 
 resultados = []
 esperados = []
-for _ in range(epochs):
+for _ in range(100):
     for fila in DATOS_PREDICCION:
         prediccion = PPN.predecir(fila)
         resultados.append(prediccion)
-    
-    esperado = map(lambda xs: xs[-1], DATOS_PREDICCION)
+
+    esperado = map(lambda row: row[-1], DATOS_PREDICCION)
     esperados = esperados + esperado
 
 print "Eficiencia: %.2f %%" % PPN.medir_performance(esperados, resultados)
-
-print PPN.propagacion_forward([17.673756466, 13.508563011, 1])
 
 #plt.plot(range(1, len(ppn.errors_)+1), ppn.errors_, marker='o')
 #plt.xlabel('Epocas')
