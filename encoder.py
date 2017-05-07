ca8c540d68e0e7d58089306ea769c7ba51953458
fbonet1 <fpbonet@gmail.com>
2017-05-07 17:25:33 -0300

    Encoder para JSONs

--- encoder.py
+++ encoder.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 import perceptron as ppn
 import io, json
+from json import JSONEncoder
 
-DATOS = [[2.7810836, 2.550537003, 0], [1.465489372, 2.362125076, 0],
-         [3.396561688, 4.400293529, 0], [1.38807019, 1.850220317, 0],
-         [3.06407232, 3.005305973, 0], [7.627531214, 2.759262235, 1],
-         [5.332441248, 2.088626775, 1], [6.922596716, 1.77106367, 1],
-         [8.675418651, -0.242068655, 1], [7.673756466, 3.508563011, 1]]
+def to_json(filepath):
+    with io.open(filepath, 'w', encoding='utf-8') as f:
+        f.write(unicode(json.dumps(PPN.__dict__, ensure_ascii=False)))
 
-N_ENTRADA = len(DATOS[0]) - 1
-RESULTADOS_ESPERADOS = [xs[-1] for xs in DATOS]
+def from_json(filepath):
+    with open(filepath, 'r') as content_file:
+        content = content_file.read()
+        json_object = json.loads(content)
 
-PPN = ppn.PerceptronMulticapa(N_ENTRADA, [3], 1, funcion_activacion="tangente", distribucion_pesos="normal", momentum=0)
-PPN.train([xs[:-1] for xs in DATOS], RESULTADOS_ESPERADOS, eta=0.05, epochs=2, tamanio_muestra_batch=1, adaptativo=False)
-
-with io.open('perceptron1.txt', 'w', encoding='utf-8') as f:
-    f.write(json.dumps(PPN, ensure_ascii=False))
+    return ppn.PerceptronMulticapa(None, None,
+                                   None, funcion_activacion=json_object['activacion_elegida'],
+                                   distribucion_pesos=json_object['distribucion'],
+                                   momentum=json_object['distribucion'],
+                                   basic_init=True, basic_init_pesos=json_object['pesos_red'])
